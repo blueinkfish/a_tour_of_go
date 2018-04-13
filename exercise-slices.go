55e765a36eca450d8bd6ea5d8f47d43fd3c1b9cc
Koichi Ono <ono@littleswitch.co.jp>
2018-04-13 16:36:33 +0900

    map

---
+++ exercise-slices.go
@@ -0,0 +1,22 @@
+package main
+
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	img := make([][]uint8, dy)
+	for y := range img {
+		img[y] = make([]uint8, dx)
+	}
+
+	for y := 0; y < dy; y++ {
+		for x := 0; x < dx; x++ {
+			img[y][x] = uint8((x + y) / 2)
+		}
+	}
+
+	return img
+}
+
+func main() {
+	pic.Show(Pic)
+}
